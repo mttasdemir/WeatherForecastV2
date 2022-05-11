9eaba2c60d565d19272a77b44f420c41cd065fcd
mttasdemir <mt.tasdemir@gmail.com>
2022-05-11 22:50:25 +0300

    Initial Commit

---
+++ WeatherForecast2/WeatherForecast2App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecast2App.swift
+//  WeatherForecast2
+//
+//  Created by Mustafa Taşdemir on 11.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecast2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
